08c83971d49c4d88cb8b0414adc0797e2c546000
ritik <ritikkharya05@gmail.com>
2024-11-16 13:43:22 +0530

    landing_page_ui_changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { BackgroundLines } from "@/components/ui/background-lines";
-import { Button } from "@/components/ui/button";
+// ParentComponent.tsx (Server Component)
 import { currentUser } from "@clerk/nextjs/server";
-import Link from "next/link";
-import LInk from "next/link";
 import { redirect } from "next/navigation";
+import LandingPage from "@/components/comments/landingpage";
+import Navbar from "@/components/Navbar";
+import AppBar from "@/components/app-bar";
 
-export default async function Landingpage() {
+export default async function ParentComponent() {
   const user = await currentUser();
 
   if (user) {
@@ -13,37 +13,9 @@ export default async function Landingpage() {
   }
 
   return (
-    <div className="flex relative items-center justify-center h-[100vh] w-full ">
-      <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
-        <h1 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-800 to-neutral-900 dark:from-neutral-600 dark:to-white text-xl  md:text-2xl lg:text-5xl font-sans py-2 md:py-10  font-bold tracking-tight fixed ">
-          Welcome to the Diary
-        </h1>
-
-        <p className="max-w-xl   text-neutral-300/90 dark:text-neutral-400 text-md gap-y-10 p-2 mt-64 text-center ">
-          Every moment, every thought — your story starts here. Share life in
-          real-time, one post at a time.
-        </p>
-
-        <div className="flex flex-row justify-around gap-4 mt-10 ">
-          {" "}
-          <Link href="/sign-in" className="z-20">
-            <Button
-              className=" text-white box-shadow  border-neutral-800 shadow-lg  hover:opacity-90 hover:scale-105 flex-none bg-card  border-2  border-slate-400/50 cursor-default"
-              variant="outline"
-            >
-              Sign in
-            </Button>
-          </Link>
-          <Link href="/sign-up" className="z-20">
-            <Button
-              className=" text-white box-shadow  border-neutral-800 shadow-lg  hover:opacity-90 hover:scale-105 flex-none bg-card  border-2  border-slate-400/50 cursor-default"
-              variant="outline"
-            >
-              Sign up
-            </Button>
-          </Link>
-        </div>
-      </BackgroundLines>
+    <div className="flex flex-col h-full items-center justify-center">
+      <AppBar />
+      <LandingPage isUserLoggedIn={!!user} />
     </div>
   );
 }
---
+++ src/components/comments/landingpage.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import Particles from "@/components/ui/particles";
+import { BorderBeam } from "@/components/ui/border-beam";
+import HeroVideoDialog from "@/components/ui/hero-video-dialog";
+import { HoverBorderGradient } from "@/components/ui/hover-border-gradient";
+import { useTheme } from "next-themes";
+
+export default function LandingPage({
+  isUserLoggedIn,
+}: {
+  isUserLoggedIn: boolean;
+}) {
+  const { themes } = useTheme();
+  const [particlesColor, setParticlesColor] = useState("dark");
+  const animatedWords = "Welcome To Diary".split(" ");
+
+  useEffect(() => {
+    setParticlesColor(themes.includes("dark") ? "#fafaf9" : "#171717");
+  }, [themes]);
+
+  if (isUserLoggedIn) return null;
+
+  return (
+    <div className="w-screen relative">
+      <div className="h-screen max-w-full mt-10">
+        <div className="p-2 flex flex-col items-center justify-center">
+          <Link href="">
+            <span className="relative z-10 text-sm">
+              <HoverBorderGradient>🌟 Join Our Community</HoverBorderGradient>
+            </span>
+          </Link>
+          <div className="leading-loose font-normal z-10 mt-10">
+            <span className="text-4xl flex items-center justify-center font-bold text-white md:text-5xl mx-auto flex-wrap w-[650px]">
+              {animatedWords.map((letter, index) => (
+                <motion.span
+                  className="px-1"
+                  key={index}
+                  initial={{ opacity: 0, filter: "blur(10px)" }}
+                  animate={{ opacity: 1, filter: "blur(0px)" }}
+                  transition={{ duration: 0.5, delay: index * 0.1 }}
+                >
+                  {letter}
+                </motion.span>
+              ))}
+            </span>
+          </div>
+          <span className="text-center mt-10 w-[650px] text-muted-foreground z-10 text-md">
+            A microblogging platform that allows you to write and share your
+            thoughts and ideas with the world.
+          </span>
+        </div>
+        <div className="relative w-[950px] mx-auto pointer-events-auto z-40 mt-24">
+          <div className="shadow-[0px_0px_300px_0px_#155e75] top-full left-0 bg-black absolute -z-10 flex max-w-screen-lg w-full aspect-video flex-col items-center justify-center overflow-hidden rounded-lg border bg-background">
+            <HeroVideoDialog
+              videoSrc="https://www.youtube.com/embed/U14GpQ5K03g"
+              thumbnailSrc="/thumbnail.jpg"
+              thumbnailAlt="Video Thumbnail"
+            />
+            <BorderBeam size={250} duration={12} delay={5} />
+          </div>
+        </div>
+      </div>
+      <Particles
+        className="absolute inset-0"
+        quantity={170}
+        ease={90}
+        color={particlesColor}
+        refresh
+      />
+    </div>
+  );
+}
