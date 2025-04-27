0377e0e5f96f368d09c979ffed44c8e9e829f9b7
ritik <ritikkharya05@gmail.com>
2025-04-27 23:35:22 +0530

    bookmark fix

--- src/app/(main)/bookmarks/page.tsx
+++ src/app/(main)/bookmarks/page.tsx
@@ -8,7 +8,10 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <main className="flex gap-4 min-w-0 w-full h-cal h-screen mb-10 gap-y-2">
-      <Bookmark />
+      <div className="flex flex-col items-center">
+        <span className="mb-4 p-3 rounded-xl shadow-sm text-neutral-300 font-mono font-bold bg-neutral-900 w-[94%] flex items-center justify-center">Bookmark</span>
+        <Bookmark />
+      </div>
     </main>
   );
 }
